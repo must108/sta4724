8ab16bb0d0ffe74d2c287b7fd5e46b20ec966ede
must108 <mustaeen18@gmail.com>
2025-08-31 03:42:36 -0400

    initial commit

---
+++ class notes/lecture 1/basics_python.py
@@ -0,0 +1,22 @@
+# i already know this ...
+
+
+x = 1
+print(type(x)) # prints int
+
+x = 2.1 # floating point number/float
+print(type(x))
+
+a = "Hello, world!"
+print(type(a))
+
+print("Hey, what's up.")
+print('Hey, what\'s up') # escape character in string
+
+print(3*"abc")
+print("abc"+"def")
+
+# bruh im done this is too boring
+
+
+
